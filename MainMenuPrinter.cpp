9aae2d872ff5707d7dad66480b9a31e393d87315
Konrad Kacper Domian <k.domian@stud.elka.pw.edu.pl>
2020-04-18 19:01:27 +0200

    Konstruktor wyswietla cale menu na terminal

---
+++ MainMenuPrinter.cpp
@@ -0,0 +1,12 @@
+#include "MainMenuPrinter.h"
+#include <iostream>
+
+void MainMenuPrinter::mainMenuPrinterPolish()
+{
+	std::cout << "MENU" << std::endl;
+	std::cout << "1.Sprzedaz" << std::endl;
+	std::cout << "2.Wymiana" << std::endl;
+	std::cout << "3.Sprawdz Kod Produktu" << std::endl;
+	std::cout << "4.Zarzadzaj Lista Produktow" << std::endl;
+	std::cout << "5.Zakoncz sprzedaz" << std::endl;
+}
