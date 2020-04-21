4b2581bef51665f0718890e482567d88c07da4c4
Konrad Kacper Domian <k.domian@stud.elka.pw.edu.pl>
2020-04-21 10:08:57 +0200

    do klasy przeniesiono z maina funkcje switch

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -1,55 +1,71 @@
 #include "MainMenu.h"
 #include "MainMenuPrinter.h"
 #include "Sprzedaz.h"
+#include "TakeFromUserData.h"
 #include <iostream>
 
 MainMenu::MainMenu()
 {
-	MainMenuPrinter menuInPolish;
-	menuInPolish.mainMenuPrinterPolish();
+	MainMenuPrinter displayMenu;
+	displayMenu.english();
+	int userChoice;
+	TakeFromUserData input(userChoice);
+	this->menuChooser(userChoice);
 }
 
 MainMenu::~MainMenu()
 {
 	system("cls");
-
 }
 
-void MainMenu::MenuswitchBox(int &input)
+void MainMenu::menuChooser(int userChoice)
 {
-	switch (input)
+	for(int i=0;i<1;i++)
 	{
-	case 1:
-	{
-		system("cls");
-		Sprzedaz sprzedaz;
-		break;
-	}
-	case 2:
-	{
+		switch (userChoice)
+		{
+		case 1:
+		{
+			system("cls");
+			std::cout << "opcja 1";
+			Sprzedaz sprzedaz; //utworz obiekt sprzedaz //bedzie sprzedaz(&doListyProduktow, &doRaportuDziennego)
+			break;
+		}
+		case 2:
+		{
+			system("cls");
+			std::cout << "opcja 2";
+			//Wymiana wymiana;
+			break;
+		}
+		case 3:
+		{
+			system("cls");
+			std::cout << "opcja 3";
+			//CheckId chceckid;
+			break;
+		}
+		case 4:
+		{
+			system("cls");
+			std::cout << "opcja 4";
+			//ProductManagement productManagement;
+			break;
+		}
+		case 5:
+		{
+			std::cout << "opcja 5"; 
+			break; //wraca do funkcji main i tym samym konczy program
+		}
+		default:
 
-		break;
-	}
-	case 3:
-	{
-
-		break;
-	}
-	case 4:
-	{
-
-		break;
-	}
-	case 5:
-	{
-
-		break;
-	}
-	default:
-	{
-		std::cout << "blad";
-		break;
-	}
+			std::cout << "mozesz wybrac jedynie opcje 1,2,3,4,5" << std::endl;
+			TakeFromUserData input(userChoice);
+			--i;
+			break;
+		}
 	}
 }
+
+
 
