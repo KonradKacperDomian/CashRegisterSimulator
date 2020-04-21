be4e1c1535dd16bf71c4a5d9a87ce81b6f31c5bc
Konrad Kacper Domian <k.domian@stud.elka.pw.edu.pl>
2020-04-21 10:07:53 +0200

    funckja switch została przeniesiona do obiektu MainMenu

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,12 @@
 #include "MainMenu.h"
-#include "TakeFromUserData.h"
 #include <iostream>
 #include <Windows.h>
 int main()
 {
-	MainMenu menu;
-	int userChoice;
-	TakeFromUserData input(userChoice);
-	menu.MenuswitchBox(userChoice);
+	//wczytywanie danych z pliku
+	//utworzenie raportu dnia
+	MainMenu menu; //utworz menu //bedzie menu(&doListyProduktow, &doRaportuDziennego)
+
+	
 	return 0;
 }
