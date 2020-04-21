2ca81d6f941155adc965d305606dd5adf661f365
Konrad Kacper Domian <k.domian@stud.elka.pw.edu.pl>
2020-04-21 10:17:39 +0200

    został utworzony plik sprzedaz.cpp, plik jest tym czasowo pusty

---
+++ Sprzedaz.cpp
@@ -0,0 +1,14 @@
+#include "Sprzedaz.h"
+#include "TakeFromUserData.h"
+#include "SprzedazMenuPrinter.h"
+#include <iostream>
+
+Sprzedaz::Sprzedaz()
+{
+	int userChoice;
+	TakeFromUserData input(userChoice);
+}
+
+
+
+
