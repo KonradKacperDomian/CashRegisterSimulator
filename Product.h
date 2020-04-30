eb1cc522564fde4e5eb2eb8f7335c96373d7fd81
Konrad Kacper Domian <k.domian@stud.elka.pw.edu.pl>
2020-05-01 00:59:44 +0200

    Struktura obiektu produkt

---
+++ Product.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+struct Product
+{
+	int productID;
+	double price;
+	char PTU; //Stawka Vat A,B,C,D,E poszczeegolnie 23%, 8%, 5%, 0%, zwolniony
+	std::string productName;
+};
+
