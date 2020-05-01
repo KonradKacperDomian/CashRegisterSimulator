0bf227d4bae2a677663d48f75819b0373e03aa22
Konrad Kacper Domian <k.domian@stud.elka.pw.edu.pl>
2020-05-02 00:56:02 +0200

    zmieniona Metode userChooser

--- SprzedazMenu.cpp
+++ SprzedazMenu.cpp
@@ -1,16 +1,48 @@
 #include "SprzedazMenu.h"
 #include "TakeFromUserData.h"
 #include "SprzedazMenuPrinter.h"
+#include "Bill.h"
+#include "MainMenu.h"
+
 
 SprzedazMenu::~SprzedazMenu()
 {
 	system("cls");
 }
 
+void SprzedazMenu::userChoice(int userChoice)
+{
+	for (int i = 0; i < 1; i++)
+	{
+		switch (userChoice)
+		{
+		case 1:
+		{
+			system("cls");
+			Bill newBill(1);
+
+			//std::cout << "opcja 1";
+			
+			break;
+		}
+		case 5:
+		{
+			//std::cout << "opcja 5";
+			MainMenu backToMenu;
+			break; //wraca do funkcji main i tym samym konczy program
+		}
+		default:
+
+			std::cout << "mozesz wybrac jedynie opcje 1,2,3,4,5" << std::endl;
+			TakeFromUserData input(userChoice);
+			--i;
+			break;
+		}
+	}
+}
+
 SprzedazMenu::SprzedazMenu()
 {
-	SprzedazMenuPrinter menu;
-	int userChoice;
 	menu.english(0);
-	TakeFromUserData input(userChoice);
+	
 }
