b4f544d9932657df9290418bb33010d177e3943c
Konrad Kacper Domian <k.domian@stud.elka.pw.edu.pl>
2020-05-03 01:34:20 +0200

    zmieniłem kolejność tworzenia obiektów

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,15 @@
 #include "MainMenu.h"
 #include <iostream>
 #include <Windows.h>
+#include "ProductListManager.h"
+#include "TakeFromUserData.h"
+#include "DailyRaport.h"
 int main()
 {
-	//wczytywanie danych z pliku
-	//utworzenie raportu dnia
+	ProductListManager VectorOfProducts;//wczytywanie danych z pliku
+	DailyRaport newDailyRaport;			//utworzenie raportu dnia
 	MainMenu menu; //utworz menu //bedzie menu(&doListyProduktow, &doRaportuDziennego)
+	menu.menuChooser(VectorOfProducts);
 
 	
 	return 0;
