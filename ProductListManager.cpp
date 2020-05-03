f7f1364c1279d5ad7c1e92c37f727626564c9021
Konrad Kacper Domian <k.domian@stud.elka.pw.edu.pl>
2020-05-04 01:11:50 +0200

    Dodano metode sprawdz czy istnije produkt jak i wprowadzono kosmetyczne zmiany w juz istniejacych

--- ProductListManager.cpp
+++ ProductListManager.cpp
@@ -28,7 +28,7 @@ ProductListManager::ProductListManager()
             std::vector<std::string> tokens;
             copy(std::istream_iterator<std::string>(iss), std::istream_iterator<std::string>(), back_inserter(tokens));
             readProduct.price=std::stod(tokens[0]);
-            readProduct.PTU = tokens[1][1];
+            readProduct.PTU = tokens[1][0];
             readProduct.productName = tokens[2];
             //std::cout << readProduct.productID << "." << readProduct.price << "." << readProduct.PTU << readProduct.productName << std::endl;
             listOfProduct.push_back(readProduct);
@@ -44,17 +44,36 @@ ProductListManager::ProductListManager()
 
 ProductListManager::~ProductListManager()
 {
-
+    std::string fileName;
+    fileName = "productsList.txt";
+    //std::cout << fileName;
+    std::ofstream myTextFile;
+    myTextFile.open(fileName.c_str(), std::ios::out | std::ios::out);
+    for (auto element : listOfProduct)
+    {
+        myTextFile << element.price << " " << element.PTU << " " << element.productName << "\n";
+  }
+    
+    myTextFile.close();
 }
 
 void ProductListManager::addProductToList()
 {
     system("cls");
+    char PTU;
     Product newProduct;
     std::cout << "Type Price of the Product:  ";
-    TakeFromUserData priceOfTheProduct(newProduct.price);
-    std::cout << "Type PTU of the Productu:  ";
-    TakeFromUserData PTUOfTheProduct(newProduct.PTU);
+    TakeFromUserData priceOfTheProduct(newProduct.price);\
+        for (;;)
+        {
+            std::cout << "Type PTU of the Productu:  ";
+            TakeFromUserData PTUOfTheProduct(PTU);
+            if (corectPTU(PTU))
+                break;
+            else
+                std::cout << "HelpDesk! PTU Table: A-23% B-8% C-5% D-0% E-NULL\nYou can type only A,B,C,D,E " << std::endl;
+        }
+    newProduct.PTU = PTU;
     std::cout << "Type nameOfTheProduct:  ";
     TakeFromUserData nameOfTheProduct(newProduct.productName);
     newProduct.productID = listOfProduct.size() + 1;
@@ -74,13 +93,11 @@ void ProductListManager::addProductToList()
             break;
         default:
             std::cout << "only (t/n)" << std::endl;
-            TakeFromUserData input(userChoice);
+            TakeFromUserData userChoice(yesOrNo);
             --i;
             break;
         }
     }
-    MainMenu menu;
-    menu.menuChooser(*this);
 }
 
 void ProductListManager::removeProductFromList()
@@ -109,13 +126,11 @@ void ProductListManager::removeProductFromList()
         default:
 
             std::cout << "only (t/n)" << std::endl;
-            TakeFromUserData input(userChoice);
+            TakeFromUserData userChoice(yesOrNo);
             --i;
             break;
         }
     }
-    MainMenu menu;
-    menu.menuChooser(*this);
 }
 
 void ProductListManager::findProduct()
@@ -124,7 +139,7 @@ void ProductListManager::findProduct()
     std::cout << "enter the product id:  ";
     int ID;
     TakeFromUserData IDs(ID);
-    if (ID-1 > listOfProduct.size()|| ID - 1 < 0)
+    if (ID > listOfProduct.size()|| ID - 1 < 0)
     {
         std::cout << "product with given id does not exist" << std::endl;
     }
@@ -137,8 +152,25 @@ void ProductListManager::findProduct()
     std::cout << "press any Key to countinue" << std::endl;
     char trash;
     TakeFromUserData Trash(trash);
-    MainMenu menu;
-    menu.menuChooser(*this);
+}
+
+bool ProductListManager::findProduct(int ID)
+{
+    system("cls");
+    bool productExist;
+    if (ID > listOfProduct.size() || ID - 1 < 0)
+    {
+        std::cout << "product with given id does not exist" << std::endl;
+        productExist = false;
+    }
+    else
+    {
+        if (listOfProduct[ID - 1].productID == 0)
+            std::cout << "product with given id does not exist" << std::endl;
+        listOfProduct[ID - 1].display(true);
+        productExist = true;
+    }
+    return productExist;
 }
 
 void ProductListManager::displayAllProductsFromList()
@@ -150,7 +182,13 @@ void ProductListManager::displayAllProductsFromList()
     std::cout << "press any Key to countinue" << std::endl;
     char trash;
     TakeFromUserData Trash(trash);
-    MainMenu menu;
-    menu.menuChooser(*this);
+}
+
+bool ProductListManager::corectPTU(char corectPTU)
+{
+    if (corectPTU == 'A' || corectPTU == 'B' || corectPTU == 'C' || corectPTU == 'D' || corectPTU == 'E')
+        return true;
+    else
+        return false;
 }
 
