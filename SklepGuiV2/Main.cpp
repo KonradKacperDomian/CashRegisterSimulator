56f2cfc8c95b826c9dbc475413930fc265d7e4b7
Konrad Kacper Domian <k.domian@stud.elka.pw.edu.pl>
2020-06-23 22:42:20 +0200

    Update Main (1).cpp

---
+++ SklepGuiV2/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <Windows.h>
+#include "core/ProductListManager.h"
+#include "core/DailyRaport.h"
+#include "gui/mainwindow.h"
+#include <QApplication>
+
+int main(int ac, char** av)
+{
+	DailyRaport newDailyRaport;
+	ProductListManager VectorOfProducts;//wczytywanie danych z pliku
+
+	QApplication app(ac, av);
+
+	MainWindow window(newDailyRaport, VectorOfProducts, nullptr);
+	window.show();
+
+	return app.exec();
+}
